363868f814080a0b5b42d52bd6c0ca0862a80025
Peter Castle <vagnerpetel2020@gmail.com>
2020-11-08 20:47:56 -0300

    some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Navbar, Nav, Form, FormControl, Button} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {WorkflowIcon, SearchIcon} from '@primer/octicons-react'
+import '../components/Header.css';
 
 
 
@@ -12,15 +13,17 @@ function Header() {
     <>
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="#home"><WorkflowIcon size={40} /></Navbar.Brand>
-          <Nav className="mr-auto">
+          <Nav>
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
-            <Button variant="outline-light"><SearchIcon size={20} /></Button>
-          </Form>
+          <Nav>
+          <Nav.Link href="#features">Features</Nav.Link>
+          </Nav> 
+          <Nav>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          
+        
         </Navbar>
         <br />
     </>)
