e78fb35f2b146def80aea6b9412ceb0d527c8d28
Peter Castle <vagnerpetel2020@gmail.com>
2020-11-21 12:02:48 -0300

    background cahnge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Header from "./components/header/Header.jsx";
 import Footer from './components/footer/Footer.jsx';
 import Home from './components/home-page/Home-page.jsx';
 import ProjectSection from './components/projectSection/ProjectSection.jsx';
@@ -8,7 +7,6 @@ import About from './components/about/About.jsx';
 function App() {
   return (
     <div>
-    <Header />
     <Home />
     <ProjectSection />
     <About />
