8c2d95ad550bf5bc2d21d51a845416eebb413593
Peter Castle <vagnerpetel2020@gmail.com>
2020-11-23 19:05:13 -0300

    changes

--- src/components/home-page/Home-page.jsx
+++ src/components/home-page/Home-page.jsx
@@ -10,37 +10,24 @@ function Home() {
     return (
         
     <div>
-        
-        <div className="nav-content" >   
-        <div className="backgroundBlur">
-        <div className="backgroundNoBlur">    
-            <h3 className="desableBlur"><a href="#" target="_blank" rel="noopener noreferrer">Portfolio</a><a href="#" target="_blank" rel="noopener noreferrer">About</a><a href="#" target="_blank" rel="noopener noreferrer">Resume</a><a href="#" target="_blank" rel="noopener noreferrer">Contact</a></h3>     
-        </div>
-        </div>
-        </div> 
         <div className="home-div">
 
-        <div className="nav-content" >   
-        <div className="backgroundBlur">
-        <div className="backgroundNoBlur">    
-            <h3 className="desableBlur"><a href="#" target="_blank" rel="noopener noreferrer">Portfolio</a><a href="#" target="_blank" rel="noopener noreferrer">About</a><a href="#" target="_blank" rel="noopener noreferrer">Resume</a><a href="#" target="_blank" rel="noopener noreferrer">Contact</a></h3>     
-        </div>
-        </div>
-        </div> 
-
-            <div className="homepage-content">
-                <img className="programming_white-img" src={programming_white} width="50%" height="50%" alt="programming_white-img"/>
-                <h1>Hi, I'm Peter</h1>
-                <h2>Etiam scelerisque quam ligula, ac <br/>porttitor justo facilisis sollicitudin.</h2> 
-                <h2>Vestibulum ipsum sem, tristique quis malesuada<br/> suscipit, ullamcorper sit amet justo.</h2> 
-                <button>Check out my work</button>
-            
-            
-            
-            
-            </div>
+            <div className="nav-content" >   
+                <h3><a href="#" target="_blank" rel="noopener noreferrer">Portfolio</a><a href="#" target="_blank" rel="noopener noreferrer">About</a><a href="#" target="_blank" rel="noopener noreferrer">Contact</a></h3>
+            </div> 
+                <div className="homepage-content">
+                    <img className="programming_white-img" src={programming_white} width="50%" height="50%" alt="programming_white-img"/>
+                    <h1>Hi, I'm Peter</h1>
+                    <h2>Etiam scelerisque quam ligula, ac <br/>porttitor justo facilisis sollicitudin.</h2> 
+                    <h2>Vestibulum ipsum sem, tristique quis malesuada<br/> suscipit, ullamcorper sit amet justo.</h2> 
+                    <button>Check out my work</button>
+                
+                
+                
+                
+                </div>
     
-    </div>
+        </div>
     </div>
     )
 }
